ad1fd683e3ab0afa2e1fb47ada9d8aa533a6e92a
vinit kumar <vinit.kumar@changer.nl>
2013-10-25 16:37:34 +0530

    style support and contact page

---
+++ socialschools_site/static/js/socialschools.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('#videos a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show');
+	});
+	$('#support a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show');
+	});  
+});
+
