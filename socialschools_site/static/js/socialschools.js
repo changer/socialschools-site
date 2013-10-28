a50128b51c20a70b5052c0789d5630985b11a200
vinit kumar <vinit.kumar@changer.nl>
2013-10-28 11:38:55 +0530

    add semicolon

--- socialschools_site/static/js/socialschools.js
+++ socialschools_site/static/js/socialschools.js
@@ -1,10 +1,10 @@
 $(function() {
   $('#videos a').click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
     $(this).tab('show');
   });
   $('#support a').click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
     $(this).tab('show');
   });  
 });
