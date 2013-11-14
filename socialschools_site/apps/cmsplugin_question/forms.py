5a74fc65c7b83892c008668972c6d4f1a701382e
Robert Korteland <robert.korteland@changer.nl>
2013-11-14 15:29:34 +0100

    add more form translations

--- socialschools_site/apps/cmsplugin_question/forms.py
+++ socialschools_site/apps/cmsplugin_question/forms.py
@@ -2,15 +2,15 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 #import settings
 from cmsplugin_question.nospam.forms import HoneyPotForm, RecaptchaForm, AkismetForm
-  
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label=_("Name"), widget=forms.TextInput(attrs={'placeholder': 'Your name*', 'class':'form-control'}))
-    email = forms.EmailField(label=_("Email"), widget=forms.TextInput(attrs={'placeholder': 'Your email address*', 'class':'form-control'}))
-    phone = forms.CharField(label=_("Phone"), widget=forms.TextInput(attrs={'placeholder': 'Phone number', 'class':'form-control'})) 
-    question = forms.CharField(label=_("Question"), widget=forms.Textarea(attrs={'placeholder': 'Question', 'class':'form-control'}))
+    name = forms.CharField(label=_("Name"), widget=forms.TextInput(attrs={'placeholder': _('Your name*'), 'class':'form-control'}))
+    email = forms.EmailField(label=_("Email"), widget=forms.TextInput(attrs={'placeholder': _('Your email address*'), 'class':'form-control'}))
+    phone = forms.CharField(label=_("Phone"), widget=forms.TextInput(attrs={'placeholder': _('Phone number'), 'class':'form-control'}))
+    question = forms.CharField(label=_("Question"), widget=forms.Textarea(attrs={'placeholder': _('Question'), 'class':'form-control'}))
 
     template = "cmsplugin_question/contact.html"
-  
+
 class HoneyPotContactForm(HoneyPotForm):
     pass
 
@@ -20,7 +20,7 @@ class AkismetContactForm(AkismetForm):
         'comment_content': 'content'
     }
     akismet_api_key = None
-    
+
 
 class RecaptchaContactForm(RecaptchaForm):
     recaptcha_public_key = None
