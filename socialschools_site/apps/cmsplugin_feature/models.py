c9f03324bd9c96189d1c28eb31b814f5c146d593
vinit Kumar <vinit.kumar@changer.nl>
2013-11-19 18:00:06 +0530

    cleanup templates properly

--- socialschools_site/apps/cmsplugin_feature/models.py
+++ socialschools_site/apps/cmsplugin_feature/models.py
@@ -14,9 +14,9 @@ class Feature(CMSPlugin):
 	float = models.CharField(_("Image placement"), max_length=10, blank=True, null=True,
         choices=FLOAT_CHOICES, help_text=_("Move image left, right or center."))
 	image = models.ImageField(_("image"), upload_to=CMSPlugin.get_media_path)
-	blue_header  = models.CharField(_("Blue part of title"),null=True, max_length=150)
+	blue_header  = models.CharField(_("Blue part of title"), max_length=150)
+	green_header = models.CharField(_("Green part of the tile"), max_length=150)
 	black_header = models.CharField(_("Black part of the tile"), max_length=150)
-	green_header = models.CharField(_("Black part of the tile"), max_length=150)
 	description = models.CharField(_("Remarks"), max_length=150)
 
 	def __unicode__(self):
