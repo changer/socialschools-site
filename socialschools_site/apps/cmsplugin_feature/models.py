4ddf06818416bbd2eb8149a89d1864763bd1023b
vinit Kumar <vinit.kumar@changer.nl>
2013-11-20 15:59:56 +0530

    cleaning models

--- socialschools_site/apps/cmsplugin_feature/models.py
+++ socialschools_site/apps/cmsplugin_feature/models.py
@@ -1,6 +1,7 @@
-from cms.models.pluginmodel import CMSPlugin
-from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+from cms.models import CMSPlugin
 from os.path import basename
 
 LEFT = "left"
@@ -14,9 +15,9 @@ class Feature(CMSPlugin):
 	float = models.CharField(_("Image placement"), max_length=10, blank=True, null=True,
         choices=FLOAT_CHOICES, help_text=_("Move image left, right or center."))
 	image = models.ImageField(_("image"), upload_to=CMSPlugin.get_media_path)
-	blue_header  = models.CharField(_("Blue part of title"), max_length=150)
-	green_header = models.CharField(_("Green part of the tile"), max_length=150)
-	black_header = models.CharField(_("Black part of the tile"), max_length=150)
+	blue_header  = models.CharField(_("Blue part of title"), max_length=150, blank=True, null=True)
+	green_header = models.CharField(_("Green part of the tile"), max_length=150, blank=True, null=True )
+	black_header = models.CharField(_("Black part of the tile"), max_length=150, blank=True, null=True)
 	description = models.CharField(_("Remarks"), max_length=150)
 
 	def __unicode__(self):
