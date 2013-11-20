fcf1146a82163dc88ac48e3c055d71a4c2f105b7
vinit Kumar <vinit.kumar@changer.nl>
2013-11-20 13:40:07 +0530

    fix the namespacing issues in three plugins

--- socialschools_site/apps/cmsplugin_price/settings.py
+++ socialschools_site/apps/cmsplugin_price/settings.py
@@ -4,6 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.conf import settings as project_settings
 
 CMSPLUGIN_PRICE_FORMS = getattr(project_settings, "CMSPLUGIN_PRICE_FORMS", (
-        ('forms.PriceForm', _('default')),
+        ('socialschools_site.apps.cmsplugin_price.forms.PriceForm', _('default')),
     )
 )
