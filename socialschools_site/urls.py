74cb974ed8e524079102d8f5a00d93c3b5b7448b
vinit Kumar <vinit.kumar@changer.nl>
2013-11-20 15:59:35 +0530

    trying to fix media path

--- socialschools_site/urls.py
+++ socialschools_site/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls.defaults import *
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
+urlpatterns =  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
