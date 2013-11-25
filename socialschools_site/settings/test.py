65be775eca92d2e5c98a41deba83dc0eb764325c
vinit Kumar <vinit.kumar@changer.nl>
2013-11-25 17:01:48 +0530

    update test setting

--- socialschools_site/settings/test.py
+++ socialschools_site/settings/test.py
@@ -3,55 +3,5 @@ import urlparse
 
 from .base import *
 
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-if "GONDOR_DATABASE_URL" in os.environ:
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
-    DATABASES = {
-        "default": {
-            "ENGINE": {
-                "postgres": "django.db.backends.postgresql_psycopg2"
-            }[url.scheme],
-            "NAME": url.path[1:],
-            "USER": url.username,
-            "PASSWORD": url.password,
-            "HOST": url.hostname,
-            "PORT": url.port
-        }
-    }
-
-STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"],"site_media", "static")
-
-STATIC_URL = "/static/" # make sure this maps inside of site_media_url
-STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"],"site_media", "static")
-
-STATIC_URL = "/static/" # make sure this maps inside of site_media_url
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "simple": {
-            "format": "%(levelname)s %(message)s"
-        },
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "simple"
-        }
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "INFO",
-    },
-    "loggers": {
-        "django.request": {
-            "propagate": True,
-        },
-    }
-}
