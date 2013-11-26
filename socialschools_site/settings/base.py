862f0548ee9bc086a77237343203a2367ac7c78b
vinit Kumar <vinit.kumar@changer.nl>
2013-11-26 16:30:38 +0530

    fixes sendgrid credentials to work on heroku

--- socialschools_site/settings/base.py
+++ socialschools_site/settings/base.py
@@ -224,8 +224,8 @@ LOGGING = {
     }
 }
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'socialschools'
-EMAIL_HOST_PASSWORD = 'Joop123#'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
