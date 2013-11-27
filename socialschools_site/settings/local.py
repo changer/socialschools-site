35f2d72eaf6f34f8cf54f267935f4676eed086bf
Robert Korteland <robert.korteland@changer.nl>
2013-11-27 14:17:48 +0100

    move sendgrid creds to production and testing settings

--- socialschools_site/settings/local.py
+++ socialschools_site/settings/local.py
@@ -2,3 +2,9 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'socialschools'
+EMAIL_HOST_PASSWORD = 'joop123'
--- socialschools_site/settings/test.py
+++ socialschools_site/settings/test.py
@@ -10,3 +10,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'assets.socialschools-www.nl'
 MEDIA_URL = 'http://assets.socialschools-www.nl.s3-website-eu-west-1.amazonaws.com/'
 
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
