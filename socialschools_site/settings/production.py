5b689c7e8641db7a71418356a39c0079b9894703
Robert Korteland <robert.korteland@changer.nl>
2013-11-27 14:27:10 +0100

    add console mail setting on local move sendgrid settings back to base

--- socialschools_site/settings/production.py
+++ socialschools_site/settings/production.py
@@ -9,10 +9,3 @@ TEMPLATE_DEBUG = DEBUG
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'assets.socialschools-www.nl'
 MEDIA_URL = 'http://assets.socialschools-www.nl.s3-website-eu-west-1.amazonaws.com/'
-
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
