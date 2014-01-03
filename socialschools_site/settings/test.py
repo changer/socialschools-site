a33e3246a5f7cc6d100591d70bef8e3bc5d0fa0e
Jos van Velzen <jos.vanvelzen@changer.nl>
2014-01-03 12:13:18 +0100

    Fix AWS settings

--- socialschools_site/settings/test.py
+++ socialschools_site/settings/test.py
@@ -1,13 +1,11 @@
 from .base import *
-from S3 import CallingFormat
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 AWS_STORAGE_BUCKET_NAME = 'assets.socialschools-www.nl'
-AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
-AWS_S3_CUSTOM_DOMAIN = 'assets.socialschools-www.nl'
 AWS_S3_SECURE_URLS = False
 AWS_PRELOAD_METADATA = True
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
