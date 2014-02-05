de79aaac24982da4e3cfc5c632c2680b5a1b9427
vinit Kumar <vinit.kumar@changer.nl>
2014-02-05 18:56:24 +0530

    cleanup code a bit for redirection based on geoi[p

--- socialschools_site/apps/geoip_redir/middleware.py
+++ socialschools_site/apps/geoip_redir/middleware.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
-
+import pygeoip
 from django.utils.functional import SimpleLazyObject
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect
 
 def get_country_request(ip):
-    import pygeoip
+    
     file_path = settings.PROJECT_ROOT + '/data/GeoIP.dat.dat'
     gi = pygeoip.GeoIP(file_path)
-    country = gi.country_name_by_addr(ip)
-    print 'I am here'    
-    if country:
-        return country
+    country = gi.country_name_by_addr(ip)   
+    return country
 
 class LocationMiddleWare(object):
-  
-     
+       
     def process_request(self, request):
     # NOTICE: This will make sure redirect loop is broken.
         if request.path in ["/en/", "/nl/"]:
@@ -23,7 +20,6 @@ class LocationMiddleWare(object):
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
         ip = request.META['REMOTE_ADDR']
-        print request.path
         country = get_country_request(ip)             
         if country == "India":
             return HttpResponsePermanentRedirect('/en/')       
