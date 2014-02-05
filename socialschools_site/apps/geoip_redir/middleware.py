2749bad750d145d35160fd9d3012e8f5172b4ad3
vinit Kumar <vinit.kumar@changer.nl>
2014-02-05 14:57:11 +0530

    test pygeoip in action

--- socialschools_site/apps/geoip_redir/middleware.py
+++ socialschools_site/apps/geoip_redir/middleware.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from django.utils.functional import SimpleLazyObject
-import pygeoip 
 
-def get_location(request):
-    
-    print request.META['REMOTE_ADDR']
+def get_country_request(ip):
+    import pygeoip
+    gi = pygeoip.GeoIP('data/GeoIp.dat.dat')
+    country = gi.country_name_by_addr(ip)
+    print 'I am here'    
+    if country:
+        print country
 
 class LocationMiddleWare(object):
-   
+  
+     
     def process_request(self, request):
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
-        print request.META['REMOTE_ADDR']    
+        ip = request.META['REMOTE_ADDR']
+        get_country_request(ip)             
         return None
