c3210e7a940e55d2c6397a3efede2351e16c5a7b
Robert Korteland <robert.korteland@changer.nl>
2014-03-31 16:30:46 +0200

    follow convention for python classname, fix weird collapse behaviour, 4space indentation everywhere

--- socialschools_site/apps/cmsplugin_faq/models.py
+++ socialschools_site/apps/cmsplugin_faq/models.py
@@ -4,9 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class faq(CMSPlugin):
 
-  question = models.CharField(_("Question"), max_length=150)
-  collapselink = models.CharField(_("Collapselink"), max_length=20)
-  answer  = models.TextField(_("Answer"))
-  image = models.ImageField(_("Image"), upload_to=CMSPlugin.get_media_path, blank=True)
-
-
+    question = models.CharField(_("Question"), max_length=150)
+    collapselink = models.CharField(_("Collapselink"), max_length=20)
+    answer  = models.TextField(_("Answer"))
+    image = models.ImageField(_("Image"), upload_to=CMSPlugin.get_media_path, blank=True)
