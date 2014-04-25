3187bead39d644c1641d9e755361883ee6f0b94f
Ruben Stolk <ruben.stolk@changer.nl>
2014-04-25 09:36:21 +0200

    Fix redirect for India

--- socialschools_site/apps/geoip_redir/middleware.py
+++ socialschools_site/apps/geoip_redir/middleware.py
@@ -5,25 +5,27 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 
 def get_country_request(ip):
-    
+
     file_path = settings.PROJECT_ROOT + '/data/GeoIP.dat.dat'
     gi = pygeoip.GeoIP(file_path)
-    country = gi.country_name_by_addr(ip)   
+    country = gi.country_name_by_addr(ip)
     return country
 
 class LocationMiddleWare(object):
-       
+
     def process_request(self, request):
     # NOTICE: This will make sure redirect loop is broken.
-        if request.path in ["/en/", "/nl/"]:
+        if request.path in ["/en/", "/nl/", "/in/"]:
             return None
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
         ip = request.META['REMOTE_ADDR']
-        country = get_country_request(ip)             
+        country = get_country_request(ip)
         if country == "India":
-            return HttpResponseRedirect('/en/')       
-        if country == "Netherlands":
+            return HttpResponseRedirect('/in/')
+        elif country == "Netherlands":
             return HttpResponseRedirect('/nl/')
+        else:
+            return HttpResponseRedirect('/en/')
         return None
 
