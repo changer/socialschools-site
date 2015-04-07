1b0be908c38ae526cc04a3afcba8311b37447472
vinit Kumar <vinit.kumar@changer.nl>
2015-04-07 17:23:46 +0530

    complete upgrade to django 1.6 and 3.0.12

--- socialschools_site/urls.py
+++ socialschools_site/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
+from cms.sitemaps import CMSSitemap
 from django.conf.urls.i18n import i18n_patterns
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.contrib import admin
 
 admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
 )
